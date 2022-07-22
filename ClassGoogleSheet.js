6fa0fba0c611b4fc9ce33908ed106c89399954a7
Puritat Chamart <108642001+introbond@users.noreply.github.com>
2022-07-22 12:18:51 +0700

    Update ClassGoogleSheet.js

--- ClassGoogleSheet.js
+++ ClassGoogleSheet.js
@@ -1,18 +1,37 @@
 class googleSheet {
   constructor(sheetName) {
-    const googleSheet = SpreadsheetApp.getActive().getSheetByName(sheetName)
+    this.package = SpreadsheetApp.getActive().getSheetByName(sheetName)
     if (googleSheet !== null) {
       this.name = sheetName
-      const columnCode = ["", "A", "B", "C", "D", "E", "F", "G", "H",
-        "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-        "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-      this.lastColumn = columnCode[googleSheet.getLastColumn()]
-      this.lastRow = googleSheet.getLastRow()
-      this.keys = googleSheet.getRange("A1:" + this.lastColumn + "1").getValues()
-      this.values = googleSheet.getRange("A2:" + this.lastColumn + this.lastRow).getValues()
+      const lastColumn = this.getColumnCode()
+      const lastRow = this.package.getLastRow()
+      const key = this.package.getRange("A1:" + lastColumn + "1").getValues()
+      this.keys = key[0]
+      this.values = this.package.getRange("A2:" + lastColumn + lastRow).getValues()
+      this.objectValues = this.getArrayOfObjects()
     }
     else {
       console.log("Error, Sheet not found.")
     }
+  }
+  getColumnCode() {
+    const columnCode = ["", "A", "B", "C", "D", "E", "F", "G", "H",
+      "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+      "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    const lastColumnIndex = this.package.getLastColumn()
+    const result = columnCode[lastColumnIndex]
+    return result
+  }
+  getArrayOfObjects() {
+    const result = []
+    let singleObject = {}
+    for (let i in this.values) {
+      for (let j in this.keys) {
+        singleObject[this.keys[j]] = this.values[i][j]
+      }
+      result.push(singleObject)
+      singleObject = {}
+    }
+    return result
   }
 }
